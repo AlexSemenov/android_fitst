20a6a101f082dcf952cd79aca642b18778138d93
Alex Semenov <mail@semenov.ru.com>
2021-01-18 22:31:38 +0300

    add HW1

--- app/src/main/java/com/ru/semenov/androidfirstapp/MainActivity.kt
+++ app/src/main/java/com/ru/semenov/androidfirstapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.ru.semenov.androidfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainButton: Button = findViewById(R.id.mainButton)
+        mainButton.setOnClickListener {
+            Log.i("@!main_btn", "Hello this is \"Main button\"")
+        }
+
+        val secondaryLeft: Button = findViewById(R.id.secondaryLeft)
+        secondaryLeft.setOnClickListener {
+            Log.i("@!secondary_btn", "Hello this is \"Secondary left button\"")
+        }
+
+        val secondaryRight: Button = findViewById(R.id.secondaryRight)
+        secondaryRight.setOnClickListener {
+            Log.i("@!secondary_btn", "Hello this is \"Secondary right button\"")
+        }
     }
 }
